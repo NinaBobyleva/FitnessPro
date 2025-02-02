8e7173676bad33c65b3e2732f18313e1b6d9d704
Nina Bobyleva <2847171@mail.ru>
2025-02-02 20:47:21 +0300

    Обновила базу данных FireBase

--- src/api/firebaseConfig.ts
+++ src/api/firebaseConfig.ts
@@ -3,20 +3,21 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVvUJPmgvz_wismeVYhzRFIH3afISoQQ8",
-  authDomain: "fitnessskyproproject.firebaseapp.com",
-  databaseURL: "https://fitnessskyproproject-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "fitnessskyproproject",
-  storageBucket: "fitnessskyproproject.appspot.com",
-  messagingSenderId: "1046783734590",
-  appId: "1:1046783734590:web:adc9c8d47f24d8dd02326f"
+  apiKey: "AIzaSyA3JspQhSXvhmqGN-4a7nxWMpU1pc2Iq8A",
+  authDomain: "skyfitnessproone.firebaseapp.com",
+  databaseURL:
+    "https://skyfitnessproone-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "skyfitnessproone",
+  storageBucket: "skyfitnessproone.firebasestorage.app",
+  messagingSenderId: "857011686778",
+  appId: "1:857011686778:web:8e4d2e79e85f1bd8f4dac3",
 };
 
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
-const db = getDatabase(app)
+const db = getDatabase(app);
 // DataBase
 
-export { auth, db, app }
+export { auth, db, app };
